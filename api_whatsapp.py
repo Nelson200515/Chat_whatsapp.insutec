98defa68894339b75683ec0b3e929236e365624c
Nelson200515 <gelsonssax@gmail.com>
2025-12-16 17:49:21 +0100

    Update api_whatsapp.py
    salvar

--- api_whatsapp.py
+++ api_whatsapp.py
@@ -1,31 +1,22 @@
 from fastapi import FastAPI, Request
 from agente.carregar_agente import carregar_agente
 
-app = FastAPI()
-
-# Carrega o agente uma única vez
-agent = carregar_agente()
-
 @app.post("/webhook")
 async def whatsapp_webhook(request: Request):
     data = await request.json()
 
     try:
-        mensagem = (
-            data["entry"][0]
-                ["changes"][0]
-                ["value"]["messages"][0]
-                ["text"]["body"]
-        )
+        message_data = data["entry"][0]["changes"][0]["value"]["messages"][0"]
+        remetente = message_data["from"]
+        mensagem = message_data["text"]["body"]
 
         resposta = agent.invoke(
             {"question": mensagem},
-            config={"configurable": {"session_id": "whatsapp"}}
+            config={"configurable": {"session_id": remetente}}
         )
 
-        return {
-            "reply": resposta["answer"]
-        }
+        return {"reply": resposta["answer"]}
 
     except Exception as e:
         return {"error": str(e)}
+
